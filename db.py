d31c5dfb6245c3d1f9b4070f8a9741e5c7ef93a2
peratop <p.leonardo7k@gmail.com>
2025-05-14 20:40:42 -0300

    css da tela de cadastro

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ def db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="PUC@1234",
+        password="pucPR@123",
         database="expcriativa"
     )
