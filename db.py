504c437719d7e6e352a868842f185036f2e93187
diegonms <diegonms06@gmail.com>
2025-05-14 21:26:46 -0300

    Alteração no login-form

--- db.py
+++ db.py
@@ -4,6 +4,6 @@ def db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="pucPR@123",
+        password="pucPR@1234",
         database="expcriativa"
     )
