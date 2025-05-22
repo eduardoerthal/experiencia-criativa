0535ba5dfde588b01f987f30e11d32b6849041f6
peratop <p.leonardo7k@gmail.com>
2025-05-22 12:55:23 -0300

    enviei no grupo o que fiz e mudei as imagens do banner

--- populardb.py
+++ populardb.py
@@ -5,7 +5,7 @@ cursor = conn.cursor()
 
 # Inserir 4 banners
 for i in range(4):
-    with open(f'./static/images/carrosel/foto{i+1}.png', 'rb') as f:
+    with open(f'./static/images/carrosel/banner{i+1}.png', 'rb') as f:
         imagem = f.read()
     cursor.execute("INSERT INTO BANNER (IMAGEM) VALUES (%s)", (imagem,))
 
